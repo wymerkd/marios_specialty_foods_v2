e62468ab68fa0cc2bca19d0c241ce9312a71d762
wymerkd <wymerkd@gmail.com>
2020-01-25 11:11:20 -0800

    add content to app_controller and create user.rb file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  def authorize
+    if !current_user
+      flash[:alert] = "You need to log in."
+    end
+  end
+
+  def user_admin
+    if !current_user
+      flash[:alert] = "You are not logged in."
+    elsif current_user.admin = false
+      flash[:alert] = "You need to be an admin to perform that action."
+    end
+  end
 end
