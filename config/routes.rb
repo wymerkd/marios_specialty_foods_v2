e283c3b5d6ad5e23cb6e4956f6fcb76b7958f1fe
wymerkd <wymerkd@gmail.com>
2020-01-25 11:15:27 -0800

    add authentication routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
   resources :products do
     resources :reviews
   end
